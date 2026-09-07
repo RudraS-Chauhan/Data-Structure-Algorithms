6d9c0579f391cebfb1d75cb40bbfeb075e944308
Rudra Singh Chauhan <rudra.dev.builds@gmail.com>
2026-09-07 13:40:51 +0530

    Time: 1 ms (99.03%) | Memory: 42.7 MB (32.04%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -3,12 +3,12 @@ class Solution {
         if(n<=0)
         return false;
 
-        int fac[] = {2,3,5};
-        for(int i =0; i<fac.length; i++)
-        {
-            while(n%fac[i] == 0)
-            n/=fac[i];
-        }
+        while(n%2 == 0)
+        n/=2;
+        while(n%3 == 0)
+        n/=3;
+        while(n%5 == 0)
+        n/=5;
         return n == 1;
     }
 }
